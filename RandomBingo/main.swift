7eb5ba108ec59bf8933a354c91353baf8cf74cfe
ksg0605 <ksg0605@gmail.com>
2023-07-13 23:02:06 +0900

    Done

--- RandomBingo/main.swift
+++ RandomBingo/main.swift
@@ -7,5 +7,25 @@
 
 import Foundation
 
-print("Hello, World!")
 
+var computerChoice = Int.random(in: 1...100)
+var myChoice: Int = 0
+
+while true {
+    var userInput = readLine()
+    
+    if let input = userInput {
+        if let number = Int(input) {
+            myChoice = number
+        }
+    }
+    
+    if computerChoice > myChoice {
+        print("Up")
+    } else if computerChoice < myChoice{
+        print("Down")
+    } else {
+        print("Bingo")
+        break
+    }
+}
